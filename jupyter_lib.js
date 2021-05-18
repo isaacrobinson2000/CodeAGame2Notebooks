cd295605bf2c199adc4394641a8da63c9c0f69dd
Isaac Robinson <awesomeisaac2000@gmail.com>
2021-05-17 23:36:32 -0600

    Done with first lesson book.

---
+++ jupyter_lib.js
@@ -0,0 +1,23 @@
+// Print to notebook cell output...
+let elem_proto = Object.getPrototypeOf(element);
+
+elem_proto.println = function(obj){
+    this.append(((obj === undefined)? "": obj) + "<br>\n");
+};
+
+elem_proto.print = function(obj){
+    this.append((obj === undefined)? "": obj);
+};
+
+elem_proto.getCanvasAndPainter = function(width, height) {
+    let jqCanvas = $.parseHTML("<canvas width='" + width + "' height='" + height +  "' style='width: 100%;'>");
+    let painter = jqCanvas[0].getContext("2d");
+    
+    this.append(jqCanvas);
+    
+    return [jqCanvas[0], painter];
+};
+
+elem_proto.makeGame = function(width, height, gameLoop) {
+    
+};
