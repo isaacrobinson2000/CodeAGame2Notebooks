aac04f57f5812946e02b2220760471d1055bc80c
Isaac Robinson <awesomeisaac2000@gmail.com>
2021-05-28 10:58:06 -0600

    Next lesson done.

--- jupyter_lib.js
+++ jupyter_lib.js
@@ -11,15 +11,84 @@ elem_proto.print = function(obj){
 
 // canvas.getBoundingClientRect() 
 // window.requestAnimationFrame
-elem_proto.getCanvasAndPainter = function(width, height) {
-    let jqCanvas = $.parseHTML("<canvas width='" + width + "' height='" + height +  "' style='width: 100%;'>");
+elem_proto.getCanvasAndPainter = function(width, height, stretch = true, heightStretch = false) {
+    let canvStyle = (stretch)? ((heightStretch)? " style='height: 75vh;'": " style='width: 100%;'"): " style='margin-right: auto; margin-left: auto; display: block;'";
+    let jqCanvas = $.parseHTML("<canvas width='" + width + "' height='" + height +  "'" + canvStyle + ">");
+    
     let painter = jqCanvas[0].getContext("2d");
+    painter.imageSmoothingEnabled = false;
     
     this.append(jqCanvas);
     
     return [jqCanvas[0], painter];
 };
 
-elem_proto.makeGame = function(width, height, gameLoop) {
+// For loading images...
+function loadImage(url) {
+    return new Promise(function(resolve, reject) {
+        let img = new Image();
+        
+        img.onload = function() {
+            resolve(img);
+        };
+        img.onerror = function() {
+            reject("Unable to load image");
+        };
+        
+        img.src = url;
+    })
+}
+
+window.loadImage = loadImage;
+
+
+function range(start = 0, stop = undefined, step = 1) {
+    let fstart = (stop != undefined)? start: 0;
+    let fstop = (stop != undefined)? stop: start;
+    let fstep = (step == 0)? 1: step;
+    
+    let numSteps = Math.floor((fstop - fstart) / fstep);
+    
+    let array = [];
+    for(let i = 0, val = fstart; i < numSteps; i++, val += fstep) {
+        array[i] = val;
+    }
+    
+    return array;
+}
+window.range = range; // Make range accessible outside this code...
+
+/* Animation:
+let spritemap = {
+    "image": "",
+    "animations": {
+        "run": {
+            "frequency": 1,
+            "frames": [0, 1, 5, 8, 3, 4],
+            "cycles": -1
+        }
+    }
+}
+*/
+
+class Sprite {
+    constructor(img, animations) {
+        this._img = img;
+        this._animations = animations;
+        this._selected = null;
+        
+        this._numSteps = Math.floor(img.width / img.height);
+        
+        this._freqOfStep = 1;
+        this._stepCounter = 1;
+        this._index = 0;
+        this._frames = range(0, this._numSteps);
+        this._cycle = true;
+    }
+    
+    
+}
+
+elem_proto.makeGame = async function(width, height, gameLoop) {
     
 };
