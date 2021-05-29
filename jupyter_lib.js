53c9db06d5c3d2ae930562504d4ed90045e89d08
Isaac Robinson <awesomeisaac2000@gmail.com>
2021-05-28 23:18:05 -0600

    New lesson, sprite rendering support.

--- jupyter_lib.js
+++ jupyter_lib.js
@@ -60,34 +60,96 @@ window.range = range; // Make range accessible outside this code...
 
 /* Animation:
 let spritemap = {
-    "image": "",
+    "image": "...",
+    "width": 30, // In pixels, default is to match image height...
     "animations": {
         "run": {
-            "frequency": 1,
-            "frames": [0, 1, 5, 8, 3, 4],
-            "cycles": -1
+            "speed": 1, // In millis
+            "frames": [0, 1, 5, 8, 3, 4], // Frames
+            "cycles": -1 // Number of cycles (negative means forever)
         }
     }
 }
 */
 
+let fdiv = (x, y) => Math.floor(x / y);
+
 class Sprite {
-    constructor(img, animations) {
+    constructor(img, width, animations) {
         this._img = img;
         this._animations = animations;
         this._selected = null;
         
-        this._numSteps = Math.floor(img.width / img.height);
+        this._height = img.height;
+        this._width = ((width <= 0) || (width == null))? this._height: width; 
         
-        this._freqOfStep = 1;
-        this._stepCounter = 1;
+        this._numImages = Math.floor(img.width / this._width);
+        
+        this._speed = 16;
+        this._frames = range(0, this._numImages);
+        this._cycles = Infinity;
+        
         this._index = 0;
-        this._frames = range(0, this._numSteps);
-        this._cycle = true;
+        this._accumulator = 0;
     }
     
+    setAnimation(animationName) {
+        self._selected = ((animationName == null) || !(animationName in this._animations))? null: animationName;
+        
+        let anim = (self._selected != null)? this._animations[self._selected]: {};
+        
+        // Update properties...
+        this._speed = ((anim.speed == null) || (anim.speed < 1))? 16: anim.speed;
+        this._frames = anim.frames ?? range(0, this._numImages);
+        this._cycles = ((anim.cycles == null) || (anim.cycles < 0))? Infinity: anim.cycles;
+        
+        for(let i = 0; i < this._frames.length; i++) {
+            this._frames[i] = Math.abs(this._frames[i]) % this._numImages;
+        }
+        
+        // Reset the index and accumulator.
+        this._index = 0;
+        this._accumulator = 0;
+    }
     
+    getAnimation() {
+        return this._selected;
+    }
+    
+    get width() {
+        return this._width;
+    }
+    
+    get height() {
+        return this._height;
+    }
+    
+    update(timeDelta) {
+        this._accumulator += timeDelta;
+        
+        // Update the index and accumulator...
+        this._index += fdiv(this._accumulator, this._speed);
+        this._accumulator %= this._speed;
+        
+        // Update the cycles based on new index.
+        this._cycles -= fdiv(this._index, this._frames.length);
+        this._index %= this._frames.length;
+        
+        if(this._cycles <= 0) {
+            this._cycles = 0;
+            this._index = this._frames.length - 1;
+        }
+    }
+    
+    draw(ctx, x, y, width, height) {
+        let imgIdx = this._frames[this._index];
+        let xin = this._width * imgIdx;
+        
+        ctx.drawImage(this._img, xin, 0, this._width, this._height, x, y, width, height);
+    }
 }
+
+window.Sprite = Sprite;
 
 elem_proto.makeGame = async function(width, height, gameLoop) {
     
